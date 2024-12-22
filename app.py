4dbec26649f8eeeaf742bd376484c639bdfc9809
JESUS MEDINA TORRES <jesus2312042@hybridge.education>
2024-12-21 22:38:59 -0600

    Subida app.py- requirements.txt- token.txt

---
+++ app.py
@@ -0,0 +1,7 @@
+from app import configurar_app
+
+# este archivo se va a encargar de poner a correr el servidor
+#aquí solamente se va a ejecutar el comando necesario para ponerlo a correr
+
+print('$ Servidor Web Programacion Avanzada  $ :D')
+configurar_app().run(port=8081)
